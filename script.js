e5e92c6d499ec82a48781b0fe619ed43adda1f10
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-06-15 20:18:04 +0530

    Added basic arithmetic functions Added basic arithmetic functions like add, subtract, divide, multiply Operate() to operate these functions from calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b){return a+b;}
+function subtract(a,b){return a-b;}
+function multiply(a,b){return a*b;}
+function divide(a,b){return a/b;}
+
+function operator(a,b,c){
+    switch(c){
+        case '+':
+            add(a,b);
+            break;
+        case '-':
+            subtract(a,b);
+            break;
+        case '+':
+            multiply(a,b);
+            break;
+        case '+':
+            divide(a,b);
+            break;
+        default:
+            return ;
+    }
+}
