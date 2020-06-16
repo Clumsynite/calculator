7df187d4fd8867b8d3c230bc8fcd833e4ecdda33
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-06-16 18:17:15 +0530

    Provisional Commit
    Trying to make a function for identifying keys and assigning them helper functions for calculator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let digits = document.querySelector('#digits');
 function add(a,b){return a+b;}
 function subtract(a,b){return a-b;}
 function multiply(a,b){return a*b;}
@@ -7,17 +8,75 @@ function operator(a,b,c){
     switch(c){
         case '+':
             return add(a,b);
-            break;
         case '-':
             return subtract(a,b);
-            break;
         case '*':
             return multiply(a,b);
-            break;
         case '/':
             return divide(a,b);
-            break;
         default:
             return ;
     }
 }
+
+function writeNumber(){
+    let numberBtn = document.querySelectorAll('button');
+    numberBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            let digitValue = digits.textContent;
+            if((btn.id).length==1){
+                if(digitValue.length<=10){
+                    if(digitValue==0){
+                        digits.textContent=btn.id;
+                    }else{
+                    digits.textContent += btn.id;
+                    console.log(digitValue);
+                    }
+                }
+            }
+        });
+    });
+}
+
+function backspace(){
+    let bkspc = document.querySelector('#backspace');
+    bkspc.addEventListener('click', () => {
+        let digitValue = digits.textContent;
+        if(digitValue.length>1){digits.textContent = (digitValue.substring(0,digitValue.length-1))} else{digits.textContent = 0};
+    });
+}
+
+//cwriteNumber();
+// backspace();
+
+
+function write(btn){
+    let digitValue = digits.textContent;
+    if(digitValue.length<=10){
+        if(digitValue==0){
+            digits.textContent=btn.id;
+        }else{
+        digits.textContent += btn.id;
+        console.log(digitValue);
+        }
+    }
+}
+function clearOne(){
+    let digitValue = digits.textContent;
+    if(digitValue.length>1){digits.textContent = (digitValue.substring(0,digitValue.length-1))} else{digits.textContent = 0};
+}
+
+function calc(){
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            if((btn.id).length==1){
+                write(btn);
+            }  
+            if(btn.id==='backspace'){
+                clearOne();
+            }  
+        });
+    });
+}
+calc();
