96504b29237f91313d3ff8ddaa72036ab0c14c61
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-06-16 18:37:37 +0530

    Improved Code | Added Negative Button

--- script.js
+++ script.js
@@ -19,63 +19,33 @@ function operator(a,b,c){
     }
 }
 
-function writeNumber(){
-    let numberBtn = document.querySelectorAll('button');
-    numberBtn.forEach(btn => {
-        btn.addEventListener('click', () => {
-            let digitValue = digits.textContent;
-            if((btn.id).length==1){
-                if(digitValue.length<=10){
-                    if(digitValue==0){
-                        digits.textContent=btn.id;
-                    }else{
-                    digits.textContent += btn.id;
-                    console.log(digitValue);
-                    }
-                }
-            }
-        });
-    });
-}
-
-function backspace(){
-    let bkspc = document.querySelector('#backspace');
-    bkspc.addEventListener('click', () => {
-        let digitValue = digits.textContent;
-        if(digitValue.length>1){digits.textContent = (digitValue.substring(0,digitValue.length-1))} else{digits.textContent = 0};
-    });
-}
-
-//cwriteNumber();
-// backspace();
-
-
 function write(btn){
-    let digitValue = digits.textContent;
-    if(digitValue.length<=10){
-        if(digitValue==0){
+    if(digits.textContent.length<=10){
+        if(digits.textContent==0){
             digits.textContent=btn.id;
         }else{
         digits.textContent += btn.id;
-        console.log(digitValue);
+        console.log(digits.textContent);
         }
     }
 }
 function clearOne(){
-    let digitValue = digits.textContent;
-    if(digitValue.length>1){digits.textContent = (digitValue.substring(0,digitValue.length-1))} else{digits.textContent = 0};
+    if(digits.textContent.length>1){digits.textContent = (digits.textContent.substring(0,digits.textContent.length-1))} else{digits.textContent = 0};
 }
-
+function ngative(){
+    if(digits.textContent.startsWith('-')){
+        digits.textContent = digits.textContent.replace('-','');
+    }else {
+        digits.textContent = '-'+digits.textContent;
+    }
+}
 function calc(){
     let buttons = document.querySelectorAll('button');
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
-            if((btn.id).length==1){
-                write(btn);
-            }  
-            if(btn.id==='backspace'){
-                clearOne();
-            }  
+            if((btn.id).length==1){write(btn);}  
+            if(btn.id==='backspace'){clearOne();}  
+            if(btn.id==='neg'){ngative();}
         });
     });
 }
