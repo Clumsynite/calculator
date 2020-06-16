ef4675f21390a98a0d3fcfce87b1c5bbba7ac773
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-06-16 20:45:41 +0530

    Added basic arithmetic functions
    Added separate functions for add, subtract
    Made a function to ease those operations

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let digits = document.querySelector('#digits');
+let calculation = document.querySelector('#calculation');
+calculation.textContent="";
+let history = [];
+
 function add(a,b){return a+b;}
 function subtract(a,b){return a-b;}
 function multiply(a,b){return a*b;}
@@ -20,13 +24,21 @@ function operator(a,b,c){
 }
 
 function writeNumber(btn){
-    if(digits.textContent.length<=10){
+    if(digits.textContent.length<=10 && calculation.textContent===""){
         if(digits.textContent==0){
             digits.textContent=btn.id;
         }else{
         digits.textContent += btn.id;
-        console.log(digits.textContent);
         }
+    }else{
+        calculation.textContent = "";
+        if(calculation!==''&&(digits.textContent>1||digits.textContent==='0')){
+            digits.textContent = '';
+        }
+        // }else if(calculation!==''&&digits.textContent==='0'){
+        //     digits.textContent = '';
+        // }
+        digits.textContent += btn.id;   
     }
 }
 function backSpace(){
@@ -46,6 +58,44 @@ function decimal(){
         digits.textContent =digits.textContent + '.';
     }
 }
+function clearScreen(){
+    digits.textContent = 0;
+    calculation.textContent = 0;
+}
+function clearAll(){
+    digits.textContent = 0;
+    calculation.textContent = '';
+    history = [];
+}
+function addBtn(){
+    if(digits.textContent==='0'){return ;}
+    history.push(digits.textContent, '+');
+    calculation.textContent = '+';
+}
+function subtractBtn(){
+    if(digits.textContent==='0'){return ;}
+    history.push(digits.textContent, '-');
+    calculation.textContent = '-';
+}
+function operation(symbol){
+    if(digits.textContent==='0'){return ;}
+    history.push(digits.textContent, symbol);
+    calculation.textContent = symbol;
+}
+function calculate(){
+    history.push(digits.textContent);
+    digits.textContent = 0;
+    let total = 0;
+    for(let i=0; i<history.length; i++){
+        if(isNaN(history[i])){
+            total += operator(Number(history[i-1]), Number(history[i+1]), history[i]);
+        }
+        if(i%2!=0){
+            
+        }
+    }
+    calculation.textContent = total;
+}
 function calc(){
     let buttons = document.querySelectorAll('button');
     buttons.forEach(btn => {
@@ -54,7 +104,11 @@ function calc(){
             if(btn.id==='backspace'){backSpace();}  
             if(btn.id==='neg'){negative();}
             if(btn.id==='decimal'){decimal();}
-            if(btn.id==='decimal'){decimal();}
+            if(btn.id==='clearScreen'){clearScreen();}
+            if(btn.id==='clearAll'){clearAll();}
+            if(btn.id==='add'){operation('+');}
+            if(btn.id==='subtract'){operation('-');}
+            if(btn.id==='equals'){calculate();}
         });
     });
 }
